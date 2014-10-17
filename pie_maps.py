50c2624715d4ae3d74b4b0c75c69831ab87f3b5c
Celita Cintas <cintas.celia@gmail.com>
2014-10-17 07:21:59 -0300

    refactor

--- pie_maps.py
+++ pie_maps.py
@@ -6,15 +6,7 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from itertools import repeat
 
-
-#TODO fix this put inside class and read number of clusters
-def get_colors(num_clusters):
-    colormap = plt.cm.gist_ncar
-    return [colormap(i) for i in np.linspace(0, 0.9, num_clusters)]
-
-colors = get_colors(75)
 
 class MainDisplay(object):
     """docstring for MainDisplay"""
@@ -24,9 +16,34 @@ class MainDisplay(object):
         plt.subplots_adjust(left=0.05,right=0.95,top=0.90,bottom=0.05,wspace=0.15,hspace=0.05)
         self.ax = plt.subplot(111)
         cid = self.fig.canvas.mpl_connect('motion_notify_event', self.onmove)
+        self.colors = self.get_colors(75) #convert to a parameter variable
 
     def onmove(self, event):
+        """ """
         pass
+
+    def get_colors(self, num_clusters):
+        """ """
+        self.colormap = plt.cm.gist_ncar
+        
+        return [self.colormap(i) for i in np.linspace(0, 0.9, num_clusters)]
+
+    def draw_pie_charts(self, label, ratios=[0.4,0.3,0.3], X=0, Y=0, size = 500):
+        """ """
+        N = len(ratios)
+        xy = []
+        start = 0.
+        for ratio in ratios:
+            x = [0] + np.cos(np.linspace(2*np.pi*start,2*np.pi*(start+ratio), 30)).tolist()
+            y = [0] + np.sin(np.linspace(2*np.pi*start,2*np.pi*(start+ratio), 30)).tolist()
+            xy1 = zip(x,y)
+            xy.append(xy1)
+            start += ratio
+     
+        for i, xyi in enumerate(xy):
+            self.ax.scatter([X],[Y] , marker=(xyi,0), s=size, facecolor=self.colors[i], alpha=0.9 )
+            self.ax.annotate(label, xy= (X, Y) , bbox = dict(boxstyle="round", ec=(1., 0.5, 0.5), fc=(1., 0.8, 0.8)))
+
 
 class MapData(object):
     """docstring for MapData"""
@@ -42,6 +59,7 @@ class MapData(object):
         self.populations_ind = self.get_populations_individuals()
         
     def get_values(self, valuename):
+        """docstring for get_values"""
         return self.df_coordinates[valuename].values
 
     def get_populations_individuals(self):
@@ -53,6 +71,7 @@ class MapData(object):
 
 
     def get_ratios(self, population):
+        """Get the proportions of each cluster in one population"""
         clusters = self.get_clusters(population)
         total_individuals = self.populations_ind[population][0]
 
@@ -60,6 +79,7 @@ class MapData(object):
 
 
     def get_clusters(self, population):
+        """distribute each individual of a population in the clusters"""
         clusters = dict(zip(range(1, 76), [0]*75))
         individuals = self.populations_ind[population][1] # This is the list of the indiviudals in the group
         for individual in individuals:
@@ -80,6 +100,7 @@ class Map(Basemap):
         #pass arg by parameter so they can change proj and other things
     
     def draw(self, color='#9A9595', alpha=0.1):
+        """ """
         #map_.bluemarble()
         #map_.shadedrelief()
         self.drawcoastlines()
@@ -91,22 +112,7 @@ class Map(Basemap):
         self.drawparallels(np.arange(-90, 90, 30))
 
 
-def draw_pie_charts(ax, label, ratios=[0.4,0.3,0.3], X=0, Y=0, size = 500):
-    """
-    """
-    N = len(ratios)
-    xy = []
-    start = 0.
-    for ratio in ratios:
-        x = [0] + np.cos(np.linspace(2*np.pi*start,2*np.pi*(start+ratio), 30)).tolist()
-        y = [0] + np.sin(np.linspace(2*np.pi*start,2*np.pi*(start+ratio), 30)).tolist()
-        xy1 = zip(x,y)
-        xy.append(xy1)
-        start += ratio
- 
-    for i, xyi in enumerate(xy):
-        ax.scatter([X],[Y] , marker=(xyi,0), s=size, facecolor=colors[i], alpha=0.9 )
-        ax.annotate(label, xy= (X, Y) , bbox = dict(boxstyle="round", ec=(1., 0.5, 0.5), fc=(1., 0.8, 0.8)))
+
         
 def main():
     """
@@ -123,7 +129,7 @@ def main():
     my_data.get_clusters(my_data.populations_ind.keys()[0])
     x,y = my_map(my_data.longitude, my_data.latitude)
     
-    map(lambda p: draw_pie_charts(my_display.ax, p[0], ratios= my_data.get_ratios(p[0]), X=p[1], Y=p[2]), zip(my_data.populations, x, y))
+    map(lambda p: my_display.draw_pie_charts(p[0], ratios= my_data.get_ratios(p[0]), X=p[1], Y=p[2]), zip(my_data.populations, x, y))
 
     
     plt.show()
